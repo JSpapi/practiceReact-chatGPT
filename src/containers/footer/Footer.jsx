966d64eb2e9f110aa80020a4e0d8cdc614b7264d
JSpapi <104095661+JSpapi@users.noreply.github.com>
2023-02-21 15:43:12 +0500

    switchedTo-mainBranch

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cl from "./Footer.module.scss";
 import logo from "../../assets/logo.svg";
+// !social links
 import { FooterList } from "../../components";
 
 const Footer = () => {
